bb25b1a96d9a912ec9e7ea8de5af504b56061363
Shopsterfied <ubuntu@ip-172-31-55-237.ec2.internal>
2016-03-21 13:46:47 +0000

    :Adding site changes before continuing

--- site/index.php
+++ site/index.php
@@ -1,3 +1,34 @@
+<?php
+
+  if (isset($_POST['user']) && isset($_POST['pass'])) {
+	  
+	  $name = $_POST['user'];
+	  $password = $_POST['pass'];
+	  $passhash = md5($password);
+	  
+	  $dbConnected = mysql_connect("localhost", "shopsterfieduser", "hrnxUuxnT57RnZmZ")
+		  or die('Failed to connect:  '.mysql_error());
+		  
+	  mysql_select_db('shopsterfied', $dbConnected)
+		  or die('Connected but could not find database: '.mysql_error());
+		  
+	  $query = "SELECT password FROM Users WHERE user_name = '$name'";
+	  $dbRecord = mysql_query($query, $dbConnected) or die("Query failed: ".mysql_error());
+	  $arrRecord = mysql_fetch_row($dbRecord);
+	  
+	  
+	  if (($passhash == $arrRecord[0]) && ($password != "")) {    
+		  header("Location: http://ec2-52-87-179-178.compute-1.amazonaws.com/shop.php"); /* Redirect browser */
+		  /* Make sure that code below does not get executed when we redirect. */
+		  exit;
+	  } else {
+		  echo '<h3>Username/Password Invalid, please try again.</h3>';
+	  }
+	  
+  } 
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +37,8 @@
     <link rel="stylesheet" href="static/css/furtive.css">
     <link rel="stylesheet" href="static/css/font-awesome.css">
     <link rel="stylesheet" href="static/css/site.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="static/js/signin.js" type="text/javascript"></script>
 </head>
 <body>
     <div class="bg--off-white py2 measure">
@@ -13,16 +46,17 @@
             <h1><i class="fa fa-shopping-bag"></i> Shopsterfied<h1>
         </div>
 
-    <form class="px2" action="#" role="form">
+    <form name="signinform" class="px2" method = "post" action="index.php" role="form">
         <label for="user">Username</label>
         <input type="text" id="user" name="user" placeholder="Username">
         <label for="pass">Password</label>
         <input type="password" id="pass" name="pass" placeholder="8 or more characters...">
         <div class="grd-row">
-            <a class="btn--gray grd-row-col-4-6" href="shop.html">Enter</a>
-            <a class="btn--link grd-row-col-2-6" href="register.html">Or Sign up...</a>
+            <a class="btn--gray grd-row-col-4-6" id="submit">Enter</a>
+            <a class="btn--link grd-row-col-2-6" href="register.php">Or Sign up...</a>
         </div>
     </form>
     </div>
 </body>
 </html>
+
