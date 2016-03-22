cbe970ee6b66e711aaa287b6d9d6b2ec2d7e2f47
ericwelz <ericwelz@comcast.net>
2016-03-21 22:58:02 -0400

    update register.js
    rewrite javascript

--- site/static/js/register.js
+++ site/static/js/register.js
@@ -6,64 +6,40 @@ $(document).ready(function(){
 	var passpattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,10}$/;
 	var msg = "";
 	
-	$('#first-name').change(function(){
-		var fname = $('#first-name').val();
-		if (fname.test(namepattern)){
-			msg = "First name OK...";
-			$('#input-msg').val(msg);
-			$('#input-msg').show();
-		}
-		else {
-			msg = "First name must start with a letter, followed by any combination of letters and/or numbers.  Minimum 1 character, maximum 40.";
-			$('#input-msg').val(msg);
-			$('#input-msg').show();
-		}
-	});
-		
-	$('#last-name').change(function(){
-		var lname = $('#last-name').val();
-		if (lname.test(namepattern)){
-			msg = "Last name OK...";
-			$('#input-msg').val(msg);
-			$('#input-msg').show();
-		}
-		else {
-			msg = "Last name must start with a letter, followed by any combination of letters and/or numbers.  Minimum 1 character, maximum 40.";
-			$('#input-msg').val(msg);
-			$('#input-msg').show();
-		}
-	});
-	
-	$('#user').change(function(){
-		var uname = $('#user').val();
-		if (uname.test(namepattern)){
-			msg = "User name OK...";
-			$('#input-msg').val(msg);
-			$('#input-msg').show();
-		}
-		else {
-			msg = "Last name must start with a letter, followed by any combination of letters and/or numbers.  Minimum 1 character, maximum 40.";
-			$('#input-msg').val(msg);
-			$('#input-msg').show();
-		}
-	});
-	
-	$('#pass').change(function(){
-		var pwrd = $('#pass').val();
-		if (pwrd.test(passpattern)){
-			msg = "Password OK...";
-			$('#input-msg').val(msg);
-			$('#input-msg').show();
-		}
-		else {
-			msg = "Password must be from 6 to 10 characters, may contain special characters, and must have at least one lowercase letter, at least one uppercase letter, and at least one number.";
-			$('#input-msg').val(msg);
-			$('#input-msg').show();
-		}
-	});
-	
 	$('#submitreg').click(function(){
-        document.forms.namedItem('regform').submit();
+		var fname = $('#first-name').val();
+		if (namepattern.test(fname)){
+			var lname = $('#last-name').val();
+			if (namepattern.test(lname)){
+				var uname = $('#user').val();
+				if (namepattern.test(uname)){
+					var pwrd = $('#pass').val();
+					if (passpattern.test(pwrd)){
+       					$('#regform').submit();
+					}
+					else{
+						msg = "First name must start with a letter, followed by any combination of letters and/or numbers.  Minimum 1 character, maximum 40.";
+						$('#input-msg').text(msg);
+						$('#input-msg').show();
+					}
+				}
+				else{
+					msg = "Last name must start with a letter, followed by any combination of letters and/or numbers.  Minimum 1 character, maximum 40.";
+					$('#input-msg').text(msg);
+					$('#input-msg').show();
+				}
+			}
+			else{
+				msg = "User name must start with a letter, followed by any combination of letters and/or numbers.  Minimum 1 character, maximum 40.";
+				$('#input-msg').text(msg);
+				$('#input-msg').show();
+			}
+		}
+		else{
+			msg = "Password must be from 6 to 10 characters, may contain special characters, and must have at least one lowercase letter, at least one uppercase letter, and at least one number.";
+			$('#input-msg').text(msg);
+			$('#input-msg').show();
+			}
     });
 		
 
