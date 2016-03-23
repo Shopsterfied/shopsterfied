357c7c5f3542fc82fba65bee81df1210c10dd6d3
ericwelz <ericwelz@comcast.net>
2016-03-23 09:53:24 -0400

    Added files via upload

--- site/static/js/shop.js
+++ site/static/js/shop.js
@@ -2,10 +2,8 @@
 
 $(document).ready(function(){
     $('#additem').click(function(){
-        var data = $('#shopform').serializeArray();
-        data.push({name: 'button', value: 'additem'});
-
-        $.post("shop.php", data).done(function(){window.location="shop.php"});	
+		$('buttonval').val('addItem');
+		$('#shopform').submit();
 		
 	});
 
