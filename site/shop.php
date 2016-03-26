78aa27e3b414029d53181af5613ceaeab8c39d18
ericwelz <ericwelz@comcast.net>
2016-03-26 18:42:26 -0400

    Added files via upload

--- site/shop.php
+++ site/shop.php
@@ -204,9 +204,9 @@
     <?php
 	
 		if ($listname != ""){
-			echo '<h5>Budget = ' . $budget . '</h5>
+			echo '<h5>Budget = $' . $budget . '</h5>
 			';
-			echo '<h5>Total Cost of items = ' . number_format($listvalue, 2) . '</h5>
+			echo '<h5>Total Cost of items = $' . number_format($listvalue, 2) . '</h5>
 			';
 		}
 	
