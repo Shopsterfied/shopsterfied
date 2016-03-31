f9e5dffc89b51a6c4f11254d12cd87b7c26fda10
ericwelz <ericwelz@comcast.net>
2016-03-31 17:56:17 -0500

    Added files via upload

--- site/static/js/shop.js
+++ site/static/js/shop.js
@@ -4,6 +4,8 @@ var listNamePattern = /^(\w{1,100})?$/;
 var itemNamePattern = /^(\w{1,80})?$/;
 var msg = "";
 
+
+
 function normalizeDollars(parameter) {
 	parameter.replace("$", "");
 	parameter.replace(",", "");
@@ -11,44 +13,18 @@ function normalizeDollars(parameter) {
 }
 
 
+function checkInput(){
 	
-	
-var isGoodListname = listNamePattern.test($('#list-name').val());
-var isGoodItemname = itemNamePattern.test($('#item-name').val());
+	var isGoodListname = ((listNamePattern.test($('#list-name').val())) || ($('#list-name').val() == ""));
+	var isGoodItemname = ((itemNamePattern.test($('#item-name').val())) || ($('#item-name').val() == ""));
 
-
-$(document).ready(function(){
-	
-	
 	if (isGoodListname){
 		if (isGoodItemname){
 			if (!isNaN(parseInt($('#priority').val())) || $('#priority').val() == ""){
 				if (!isNaN(normalizeDollars($('#price').val())) || $('#price').val() == ""){
 					if (!isNaN(parseInt($('#quantity').val())) || $('#quantity').val() == ""){
 						if (!isNaN(normalizeDollars($('#budget').val())) || $('#budget').val() == ""){
-							
-							//If Add Item/Update List was clicked
-							$('#additem').click(function(){
-								$('#buttonval').val('addItem');
-								$('#shopform').submit();
-								
-							});
-							
-							//If start shopping was clicked
-							$('#shopnow').click(function(){
-								$('#buttonval').val('shopnow');
-								$('#shopform').submit();
-								
-							});
-							
-							//If Clear List was clicked
-							$('#clear').click(function(){
-								if (confirm("You have chosen to clear and delete this list.  Are you sure?")){
-									$('#buttonval').val('clear');
-									$('#shopform').submit();
-								}
-							});
-							
+							return true;
 						}
 						else{
 							msg = "Budget input is invalid";
@@ -85,4 +61,37 @@ $(document).ready(function(){
 		$('#input-msg').text(msg);
 		$('#input-msg').show();
 	}
+	return false;
+}// end function checkInput
+
+	
+	
+
+
+$(document).ready(function(){
+	
+	//If Add Item/Update List was clicked
+	$('#additem').click(function(){
+		if (checkInput()){
+			$('#buttonval').val('addItem');
+			$('#shopform').submit();
+		}
+	});
+	
+	//If start shopping was clicked
+	$('#shopnow').click(function(){
+		if (checkInput()){
+			$('#buttonval').val('shopnow');
+			$('#shopform').submit();
+		}
+	});
+	
+	//If Clear List was clicked
+	$('#clear').click(function(){
+		if (confirm("You have chosen to clear and delete this list.  Are you sure?")){
+			$('#buttonval').val('clear');
+			$('#shopform').submit();
+		}
+	});		
+			
 });
